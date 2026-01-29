eeff28f9a20f428c734c71e95c5c0292275027e6
Bilal Zaidi <tombilal53@gmail.com>
2026-01-28 21:47:37 -0500

    Update MyStack.java

--- MyStack.java
+++ MyStack.java
@@ -1,7 +1,3 @@
-package assignment2;
-
-import java.util.EmptyStackException;
-
 public class MyStack<E> {
 	private SNode<E> head;	
 	private	int size; 	
@@ -87,3 +83,4 @@ public class MyStack<E> {
 	}
 
 }
+
